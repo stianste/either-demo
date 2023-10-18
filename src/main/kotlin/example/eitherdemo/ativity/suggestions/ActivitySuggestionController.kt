41a2fe8ea9945d77bcd20db6a2791dbab2663af5
Stian Steinbakken <stian.steinbakken@posten.no>
2023-10-18 14:41:31 +0200

    refactor: add explicity types for clarity

--- src/main/kotlin/example/eitherdemo/ativity/suggestions/ActivitySuggestionController.kt
+++ src/main/kotlin/example/eitherdemo/ativity/suggestions/ActivitySuggestionController.kt
@@ -1,5 +1,8 @@
 package example.eitherdemo.ativity.suggestions
 
+import example.eitherdemo.ativity.suggestions.models.external.api.BoredApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,5 +15,6 @@ class ActivitySuggestionController(
   @GetMapping("/activity-suggestion")
   fun getActivitySuggestion(
     @RequestParam activityType: String,
-  ) = activitySuggestionService.getActivitySuggestion(activityType)
+  ): ResponseEntity<BoredApiResponse?> =
+    ok().body(activitySuggestionService.getActivitySuggestion(activityType))
 }
