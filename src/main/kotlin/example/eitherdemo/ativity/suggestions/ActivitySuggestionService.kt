ee54ed2d55cb63390306fe252ff821adeca08407
Stian Steinbakken <stian.steinbakken@posten.no>
2023-10-18 14:41:31 +0200

    refactor: add explicity types for clarity

--- src/main/kotlin/example/eitherdemo/ativity/suggestions/ActivitySuggestionService.kt
+++ src/main/kotlin/example/eitherdemo/ativity/suggestions/ActivitySuggestionService.kt
@@ -1,8 +1,10 @@
 package example.eitherdemo.ativity.suggestions
 
+import example.eitherdemo.ativity.suggestions.models.external.api.BoredApiResponse
 import org.springframework.stereotype.Service
 
 @Service
 class ActivitySuggestionService(private val apiClient: BoredApiClient) {
-  fun getActivitySuggestion(activityType: String) = apiClient.fetchActivitySuggestion(activityType)
+  fun getActivitySuggestion(activityType: String): BoredApiResponse? =
+    apiClient.fetchActivitySuggestion(activityType)
 }
