68337e525c55022f64cf3ad7cdea1efba7d463a4
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-13 11:26:25 +0100

    feat: add complete sealed interface example

--- src/main/kotlin/example/sealedclasses/App.kt
+++ src/main/kotlin/example/sealedclasses/App.kt
@@ -4,9 +4,12 @@ class App {
   companion object {
     fun handleError(exception: AppException): Int =
       when (exception) {
-        is AppException.ThatFirstExternalApiException,
-        is AppException.ThatSecondExternalApiException -> -1
+        is AppException.ClientError -> {
+          val wellDefinedStatus = exception.httpStatus!! // redundant now
+          wellDefinedStatus
+        }
         is AppException.ThatDomainExceptionWhichNeedsSpecialAttention -> {
+          val errors = exception.errors
           println("Would do some special handling here")
           200
         }
--- src/main/kotlin/example/sealedclasses/AppException.kt
+++ src/main/kotlin/example/sealedclasses/AppException.kt
@@ -1,45 +1,37 @@
 package example.sealedclasses
 
-class AppException(
-  val message: String,
-  val cause: Throwable? = null,
-  val httpStatus: Int? = null,
-  val thoseDomainExceptionsWhichDontHaveAnythingToDoWithExternalApis: List<DomainException>? = null,
-) {
-  class ThatFirstExternalApiException(
-    cause: Throwable,
-    httpStatus: Int? = null,
-  ) :
-    AppException(
-      message =
-        cause.message
-          ?: "Something went wrong when calling the first external api, and no message was provided.",
-      cause = cause,
-      httpStatus = httpStatus,
-    )
+sealed interface AppException {
+  val message: String
 
-  class ThatSecondExternalApiException(
-    cause: Throwable,
-    httpStatus: Int? = null,
-  ) :
-    AppException(
-      message =
-        cause.message
-          ?: "Something went wrong when calling the second external api, and no message was provided.",
-      cause = cause,
-      httpStatus = httpStatus,
-    )
+  sealed interface ClientError : AppException {
+    val cause: Throwable
+    val httpStatus: Int
+  }
 
-  class ThatDomainExceptionWhichNeedsSpecialAttention(
-    errors: List<DomainException>,
-  ) :
-    AppException(
-      message =
-        if (errors.contains(DomainException.BasicallyExpected)) "Pjuh"
-        else "OMG the world is burning",
-      cause = null,
-      httpStatus = if (errors.contains(DomainException.BasicallyExpected)) 200 else 500,
-    )
+  data class ThatFirstExternalApiException(
+    override val cause: Throwable,
+    override val httpStatus: Int,
+  ) : ClientError {
+    override val message: String =
+      cause.message
+        ?: "Something went wrong when calling the first external api, and no message was provided."
+  }
+
+  data class ThatSecondExternalApiException(
+    override val cause: Throwable,
+    override val httpStatus: Int,
+  ) : ClientError {
+    override val message: String =
+      cause.message
+        ?: "Something went wrong when calling the second external api, and no message was provided."
+  }
+
+  data class ThatDomainExceptionWhichNeedsSpecialAttention(
+    val errors: List<DomainException>,
+  ) : AppException {
+    override val message =
+      if (errors.contains(DomainException.BasicallyExpected)) "Pjuh" else "OMG the world is burning"
+  }
 }
 
 enum class DomainException {
