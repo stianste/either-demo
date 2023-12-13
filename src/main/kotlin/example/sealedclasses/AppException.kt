df181b2f17f6b1f20f1a1e6d16c6b329d99286a5
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-13 09:51:15 +0100

    chore: add starting point for sealed class example

---
+++ src/main/kotlin/example/sealedclasses/AppException.kt
@@ -0,0 +1,48 @@
+package example.sealedclasses
+
+class AppException(
+  val message: String,
+  val cause: Throwable? = null,
+  val httpStatus: Int? = null,
+  val thoseDomainExceptionsWhichDontHaveAnythingToDoWithExternalApis: List<DomainException>? = null,
+) {
+  class ThatFirstExternalApiException(
+    cause: Throwable,
+    httpStatus: Int? = null,
+  ) :
+    AppException(
+      message =
+        cause.message
+          ?: "Something went wrong when calling the first external api, and no message was provided.",
+      cause = cause,
+      httpStatus = httpStatus,
+    )
+
+  class ThatSecondExternalApiException(
+    cause: Throwable,
+    httpStatus: Int? = null,
+  ) :
+    AppException(
+      message =
+        cause.message
+          ?: "Something went wrong when calling the second external api, and no message was provided.",
+      cause = cause,
+      httpStatus = httpStatus,
+    )
+
+  class ThatDomainExceptionWhichNeedsSpecialAttention(
+    errors: List<DomainException>,
+  ) :
+    AppException(
+      message =
+        if (errors.contains(DomainException.BasicallyExpected)) "Pjuh"
+        else "OMG the world is burning",
+      cause = null,
+      httpStatus = if (errors.contains(DomainException.BasicallyExpected)) 200 else 500,
+    )
+}
+
+enum class DomainException {
+  BasicallyExpected,
+  TotalDisaster,
+}
